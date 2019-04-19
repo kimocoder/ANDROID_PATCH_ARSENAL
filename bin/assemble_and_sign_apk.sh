e1c30389140f47c68478b7def2fb474e18137c00
kimocoder <christian@aircrack-ng.org>
2019-04-19 16:47:38 +0200

    Refactor

---
+++ bin/assemble_and_sign_apk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$ANDROID_HOME" ];then
+    export ANDROID_HOME=$PWD/sdk
+fi
+
+gradleTarget=assembleDebug
+target=debug
+file=app-debug
+if [ "$1" == "release" ];then
+    gradleTarget=assembleRelease
+    target=release
+    file=app-release-unsigned
+fi
+./gradlew $gradleTarget
+LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 ./app/build/outputs/apk/$target/${file}.apk app.apk
